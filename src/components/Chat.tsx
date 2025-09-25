8c8d2b6846a52a2fa77af6016d21b00e26935c2c
Chris Nicholas <ctnicholasdev@gmail.com>
2025-09-25 14:28:29 +0100

    feat: Streaming

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,7 +2,9 @@ import { RegisterAiKnowledge, RegisterAiTool } from "@liveblocks/react";
 import { AiChat } from "@liveblocks/react-ui";
 import { useSpreadsheet } from "../spreadsheet/react";
 import { convertSpreadsheetToKeyValueArray } from "../spreadsheet/utils";
-import { defineAiTool } from "@liveblocks/client";
+import { defineAiTool, JsonObject } from "@liveblocks/client";
+import { AiToolInvocationProps } from "@liveblocks/core";
+import { useRef } from "react";
 
 export function Chat() {
   const spreadsheet = useSpreadsheet();
@@ -18,13 +20,13 @@ export function Chat() {
     cells
   );
 
-  console.log(
-    spreadsheetData,
-    columns.map((column, index) => ({
-      id: column.id,
-      columnLetter: String.fromCharCode(65 + index),
-    }))
-  );
+  // console.log(
+  //   spreadsheetData,
+  //   columns.map((column, index) => ({
+  //     id: column.id,
+  //     columnLetter: String.fromCharCode(65 + index),
+  //   }))
+  // );
 
   return (
     <>
@@ -56,15 +58,16 @@ export function Chat() {
             required: ["cells"],
             additionalProperties: false,
           },
-          execute: async ({ cells }) => {
-            console.log("executing", cells);
-            cells?.forEach(
-              (cell: { rowId: string; columnId: string; value: string }) => {
-                setCellValue(cell.columnId, cell.rowId, cell.value);
-                console.log("setting", cell);
-              }
-            );
-          },
+          // execute: async ({ cells }) => {
+          //   console.log("executing", cells);
+          //   cells?.forEach(
+          //     (cell: { rowId: string; columnId: string; value: string }) => {
+          //       setCellValue(cell.columnId, cell.rowId, cell.value);
+          //       console.log("setting", cell);
+          //     }
+          //   );
+          // },
+          render: RenderEditCellsTool,
         })}
       />
 
@@ -89,4 +92,70 @@ export function Chat() {
       />
     </>
   );
+}
+
+function RenderEditCellsTool({
+  stage,
+  partialArgs,
+  args,
+  respond,
+}: AiToolInvocationProps<
+  {
+    cells: {
+      rowId: string;
+      columnId: string;
+      value: string;
+    }[];
+  },
+  JsonObject
+>) {
+  const lastEditedCell = useRef<{
+    rowId: string;
+    columnId: string;
+    value: string;
+  }>();
+
+  const spreadsheet = useSpreadsheet();
+  if (!spreadsheet) return null;
+  const { setCellValue } = spreadsheet;
+
+  if (stage === "receiving") {
+    if (
+      !partialArgs.cells ||
+      !Array.isArray(partialArgs.cells) ||
+      partialArgs.cells?.length === 0
+    ) {
+      return;
+    }
+
+    const current = partialArgs.cells[partialArgs.cells.length - 1] as
+      | {
+          rowId: string;
+          columnId: string;
+          value: string;
+        }
+      | undefined;
+
+    if (current) {
+      setTimeout(() =>
+        setCellValue(current.columnId, current.rowId, current.value)
+      );
+    }
+
+    return <div>Edit Cells Tool</div>;
+  }
+
+  if (stage === "executing") {
+    // args.cells.forEach(
+    //   (cell: { rowId: string; columnId: string; value: string }) => {
+    //     setTimeout(() => setCellValue(cell.columnId, cell.rowId, cell.value));
+    //     console.log("setting", cell);
+    //   }
+    // );
+    // console.log("setCellValue");
+    respond({ data: {}, description: "Cells edited" });
+    return;
+  }
+
+  return <div>Edit Cells Tool</div>;
 }
